c1f82e6c62883a97750a4c06fe2cc5da1cdeb1c7
Jack den Ouden <jackldam@gmail.com>
2021-02-28 15:16:16 +0100

    ReadyForRelease

--- PSModuleBuilder.ps1
+++ PSModuleBuilder.ps1
@@ -1,100 +1,114 @@
 <#
     .SYNOPSIS
-        Short description
+        This will allow you to quickly create your own custom Powershell Module's
     .DESCRIPTION
-        Long description
+        This will allow you to quickly create your own custom Powershell Module's
+        Just select a Source Path were you saved all your functions in separate .ps1 files
+        and select an output filePath.
+
+        And this script will do the rest.
     .EXAMPLE
-        PS C:\> <example usage>
-        Explanation of what the example does
+        PS C:\> & ".\PSModuleBuilder.ps1" -SourcePath ".\Functions" -FilePath ".\Test.psm1" -ModifiedBy "Jack den Ouden"
+        PS C:\>
     .INPUTS
-        Inputs (if any)
+        Source Path where all *.ps1 files are located
     .OUTPUTS
-        Output (if any)
+        a *.psm1 File specified at the location
     .NOTES
-        General notes
+        2021-02-28 Jack den Ouden Created and updated this Script.
     #>
 [CmdletBinding()]
 param (
     # Parameter help description
-    [Parameter(Mandatory = $true)]
+    [Parameter(Position = 0, Mandatory = $true)]
     [string]
-    $Source,
+    $SourcePath,
     # Parameter help description
-    [Parameter(Mandatory = $true)]
+    [Parameter(Position = 1, Mandatory = $true)]
     [string]
-    $Destination,
+    $FilePath,
     # Parameter help description
-    [Parameter(Mandatory = $true)]
+    [Parameter(Position = 2, Mandatory = $false)]
     [string]
-    $Name,
-    # Parameter help description
-    [Parameter(Mandatory = $false)]
-    [Switch][bool]
-    $Force
-        
+    $ModifiedBy = $env:USERNAME
 )
 
+#Region PreChecks
+
+#Perform first tests to see if script can actually work.
+#Test if SourcePath exists if not throw error
+$TestPath = Test-Path -Path:$(Split-Path $SourcePath)
+if (!$TestPath) {
+    Throw "Source path not found `"$($SourcePath)`""
+}
+
+#Test if Path exists if not throw error
+$TestPath = Test-Path -Path:$(Split-Path $FilePath -Parent)
+if (!$TestPath) {
+    Throw "Destination Path not found `"$(Split-Path $FilePath -Parent)`""
+}
+    
+#endregion
+
+#We want to store a temp file on a local location prior to writing the complete module to the destination.
+#This will ensure a complete file.
+
 #Create TemfileVariable
-$TemfileVariable = "$($env:TEMP)\$Name"
+$TemfileVariable = "$($env:TEMP)\$((New-Guid).Guid)"
 
 #Get Source folder content.
 Write-Verbose "SourceFolder `"$Source`""
-$ListFiles = $((Get-ChildItem -Path:$Source -Filter:"*.ps1").FullName)
+$ListFiles = $((Get-ChildItem -Path:$SourcePath -Filter:"*.ps1").FullName)
 
 #If Filename doesn't contain correct extension it will be added.
 if (-not ($name -like "*.psm1")) {
     $name = "$name.psm1"
 }
-        
 
-#test if an module with name already exists
-if (Test-Path -Path:"$Destination\$Name") {
-    if (($Force)) {
-        Remove-Item "$Destination\$Name" -Force
-    }
-    else {
-        Write-Verbose "$("$Destination\$Name") Already exists and no force parameter has been used"
-        exit
-    }
-}
+
 
 #Default parameters for writing to file
 $Params = @{
-    Path  = "$TemfileVariable"
-    Force = $true
+    FilePath = "$TemfileVariable"
+    Encoding = "utf8"
 }
-#Add info about last update and whome pushed it.
-Set-Content @Params -Value:"<$([char]0x0023)`nUpdated: $(Get-Date)`nPerformed by: $($env:USERNAME)`n$([char]0x0023)>"
+
+#Add info about last update and who modified it.
+Out-File @Params -InputObject:"<$([char]0x0023)`nUpdated: $(Get-Date)`nModified by: $($ModifiedBy)`n$([char]0x0023)>"
+
+#Add Append Parameter to the Params so we won't overwrite the file but just add data.
+$Params.Add("Append", $true)
+
 #Add a new line
-Add-Content @Params -Value:""
+Out-File @Params -InputObject:""
 
 #Test if Source folder is empty.
 if (-not ($null -eq $ListFiles)) {
     $ListFiles | ForEach-Object {
         $FunctionName = $((Split-Path -Path:$_ -Leaf).Replace(".ps1", ""))
         Write-Verbose "Store function $FunctionName"
+        
         #Add #Region <Variablename> line
-        Add-Content @Params -Value:"$([char]0x0023)region $FunctionName"
-        #Start-Sleep -Milliseconds:"100"
+        Out-File @Params -InputObject:"$([char]0x0023)region $FunctionName"
+                
         #Add Function to Module
-        Add-Content @Params -Value:$(Get-Content -Path:"$_" -Raw)
+        Out-File @Params -InputObject:$(Get-Content -Path:"$_" -Raw)
+        
         #Add #EndRegion <Variablename> line
-        Add-Content @Params -Value:"$([char]0x0023)endregion $FunctionName"
+        Out-File @Params -InputObject:"$([char]0x0023)endregion $FunctionName"
+        
         #Add a new line
-        Add-Content @Params -Value:""
+        Out-File @Params -InputObject:""
                 
     }
-    Write-Verbose "Copying $TemfileVariable to $Destination\$Name"
-    if (-not (Test-Path "$Destination")) {
-        New-Item -Path:$Destination -ItemType:Directory -Force
-    }
 
-    Copy-Item -Path:$TemfileVariable -Destination:"$Destination\$Name" -Force
+    #Copy the temporaryfile to the Destination.
+    Copy-Item -Path:$TemfileVariable -Destination:$FilePath
+
+    #Remove the temporaryfile to keep everything nice and tidy.
+    Remove-Item -Path:"$TemfileVariable"
 }
 else {
     Write-Verbose "No Functions found to import"
     exit
 }
-
-
-
