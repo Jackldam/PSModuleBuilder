dc9b2a4e512b79c2557230341216e2b44ba39a87
Jackldam <54206590+Jackldam@users.noreply.github.com>
2021-03-05 15:41:13 +0100

    Update PSModuleBuilder.ps1
    Changed in to a function so you can run the script as and save your params

--- PSModuleBuilder.ps1
+++ PSModuleBuilder.ps1
@@ -1,114 +1,250 @@
-<#
+function Update-Module {
+    <#
     .SYNOPSIS
-        This will allow you to quickly create your own custom Powershell Module's
+        Short description
     .DESCRIPTION
-        This will allow you to quickly create your own custom Powershell Module's
-        Just select a Source Path were you saved all your functions in separate .ps1 files
-        and select an output filePath.
+        Given a version number Major.Minor.Build, increment the:
 
-        And this script will do the rest.
+        Major version when you make incompatible changes,
+        Minor version when you add functionality in a backwards compatible manner, and
+        Build version when you make backwards compatible bug fixes.
     .EXAMPLE
-        PS C:\> & ".\PSModuleBuilder.ps1" -SourcePath ".\Functions" -FilePath ".\Test.psm1" -ModifiedBy "Jack den Ouden"
-        PS C:\>
+        PS C:\> <example usage>
+        Explanation of what the example does
     .INPUTS
-        Source Path where all *.ps1 files are located
+        Inputs (if any)
     .OUTPUTS
-        a *.psm1 File specified at the location
+        Output (if any)
     .NOTES
-        2021-02-28 Jack den Ouden Created and updated this Script.
+        General notes
     #>
-[CmdletBinding()]
-param (
-    # Parameter help description
-    [Parameter(Position = 0, Mandatory = $true)]
-    [string]
-    $SourcePath,
-    # Parameter help description
-    [Parameter(Position = 1, Mandatory = $true)]
-    [string]
-    $FilePath,
-    # Parameter help description
-    [Parameter(Position = 2, Mandatory = $false)]
-    [string]
-    $ModifiedBy = $env:USERNAME
-)
+    [CmdletBinding()]
+    param (
+        # Parameter help description
+        [Parameter(Mandatory = $true)]
+        [string]
+        $Functions,
+        # Parameter help description
+        [Parameter(Mandatory = $true)]
+        [string]
+        $Destination,
+        # Parameter help description
+        [Parameter(Mandatory = $true)]
+        [string]
+        $ModuleName,
+        # Parameter help description
+        [Parameter()]
+        [ValidateSet('None', 'Build', 'Minor', "Major")]
+        [string]
+        $Increment
+    )
 
-#Region PreChecks
+    try { Set-ExecutionPolicy -ExecutionPolicy:"Bypass" -Scope:"Process" }catch {}
 
-#Perform first tests to see if script can actually work.
-#Test if SourcePath exists if not throw error
-$TestPath = Test-Path -Path:$(Split-Path $SourcePath)
-if (!$TestPath) {
-    Throw "Source path not found `"$($SourcePath)`""
-}
-
-#Test if Path exists if not throw error
-$TestPath = Test-Path -Path:$(Split-Path $FilePath -Parent)
-if (!$TestPath) {
-    Throw "Destination Path not found `"$(Split-Path $FilePath -Parent)`""
-}
+    #Functions used By PSModulebuilder.
+    function Update-FileVersion {
+        <#
+        .SYNOPSIS
+            A FileVersion update function that will allow you to quickly update a file using Semantic Versioning.
+        .DESCRIPTION
+            A FileVersion update function that will allow you to quickly update a file using Semantic Versioning.
     
-#endregion
-
-#We want to store a temp file on a local location prior to writing the complete module to the destination.
-#This will ensure a complete file.
-
-#Create TemfileVariable
-$TemfileVariable = "$($env:TEMP)\$((New-Guid).Guid)"
-
-#Get Source folder content.
-Write-Verbose "SourceFolder `"$Source`""
-$ListFiles = $((Get-ChildItem -Path:$SourcePath -Filter:"*.ps1").FullName)
-
-#If Filename doesn't contain correct extension it will be added.
-if (-not ($name -like "*.psm1")) {
-    $name = "$name.psm1"
-}
-
-
-
-#Default parameters for writing to file
-$Params = @{
-    FilePath = "$TemfileVariable"
-    Encoding = "utf8"
-}
-
-#Add info about last update and who modified it.
-Out-File @Params -InputObject:"<$([char]0x0023)`nUpdated: $(Get-Date)`nModified by: $($ModifiedBy)`n$([char]0x0023)>"
-
-#Add Append Parameter to the Params so we won't overwrite the file but just add data.
-$Params.Add("Append", $true)
-
-#Add a new line
-Out-File @Params -InputObject:""
-
-#Test if Source folder is empty.
-if (-not ($null -eq $ListFiles)) {
-    $ListFiles | ForEach-Object {
-        $FunctionName = $((Split-Path -Path:$_ -Leaf).Replace(".ps1", ""))
-        Write-Verbose "Store function $FunctionName"
+            Given a version number Major.Minor.Build, increment the:
+    
+                Major version when you make incompatible changes,
+                Minor version when you add functionality in a backwards compatible manner, and
+                Build version when you make backwards compatible bug fixes.
+    
+            Source https://semver.org/
+        .EXAMPLE
+            PS C:\> Update-FileVersion -FileName:"TestVersion.1.1.1.ps1" -Build
+            TestVersion.1.1.2.ps1
+        .EXAMPLE
+            PS C:\> Update-FileVersion -FileName:"TestVersion.1.1.1.ps1" -Minor
+            TestVersion.1.2.0.ps1
+        .EXAMPLE
+            PS C:\> Update-FileVersion -FileName:"TestVersion.1.1.1.ps1" -Major
+            TestVersion.2.0.0.ps1
+        .INPUTS
+            FileName: string
+            IncrementalType: Major', 'Minor', "Build"
+        .OUTPUTS
+            String
+        .NOTES
+            2021-01-29 Jack den Ouden Function Created
+        #>
+        param (
+            [Parameter(Mandatory = $true)]
+            [ValidateNotNullOrEmpty()]
+            [string]
+            $FileName,
+            [Parameter(ParameterSetName = 'Build', Mandatory = $true)]
+            [Switch]
+            $Build,
+            [Parameter(ParameterSetName = 'Minor', Mandatory = $true)]
+            [Switch]
+            $Minor,
+            [Parameter(ParameterSetName = 'Major', Mandatory = $true)]
+            [Switch]
+            $Major
+        )
+    
+        #Using regex to split the name in to 3 groups
+        #Fist group everyting prior to
+        #Number('s) dot Number('s) dot Number('s)
+        $PreVersion = $([regex]::Match($FileName, '(.*\D)(\d+\.\d+\.\d+)(.*)').Groups[1].value)
+        #Second group version number can be
+        #Number('s) dot Number('s) dot Number('s)
+        [version]$Version = $([regex]::Match($FileName, '(.*\D)(\d+\.\d+\.\d+)(.*)').Groups[2].value)
+        #Third group anything after
+        ##Number('s) dot Number('s) dot Number('s)
+        $PostVersion = $([regex]::Match($FileName, '(.*\D)(\d+\.\d+\.\d+)(.*)').Groups[3].value)
         
-        #Add #Region <Variablename> line
-        Out-File @Params -InputObject:"$([char]0x0023)region $FunctionName"
-                
-        #Add Function to Module
-        Out-File @Params -InputObject:$(Get-Content -Path:"$_" -Raw)
+        switch ($PSCmdlet.ParameterSetName) {
+            Build { $Version = "{0}.{1}.{2}" -f $($Version.Major), $($Version.Minor), $($Version.Build + 1) }
+            Minor { $Version = "{0}.{1}.{2}" -f $($Version.Major), $($Version.Minor + 1), 0 }
+            Major { $Version = "{0}.{1}.{2}" -f $($Version.Major + 1), 0, 0 }
+        }
         
-        #Add #EndRegion <Variablename> line
-        Out-File @Params -InputObject:"$([char]0x0023)endregion $FunctionName"
+        return "$PreVersion" + "$Version" + "$PostVersion"
+    }
+    
+    Function Get-MostRecentFile {
+        <#
+        .SYNOPSIS
+            Get-MostRecentFile allow's you to get the most recent file in a folder.
+        .DESCRIPTION
+            Get-MostRecentFile allow's you to get the most recent file in a folder.
+            When you have multiple files with similar names you can also filter on a part of the name to make sure you get the correct file
+        .EXAMPLE
+            PS C:\> Get-MostRecentFile -Path "C:\Test" -Filter ""
+            Explanation of what the example does
+        .INPUTS
+            Inputs (if any)
+        .OUTPUTS
+            Output (if any)
+        .NOTES
+            2021-01-21 Jack den Ouden Created Function
+        #>
+        [CmdletBinding()]
+        param (
+            [Parameter()]
+            [String]
+            $Path,
+            [Parameter()]
+            [String]
+            $Filter
+        )
+    
+        $Params = @{
+            Path   = $Path
+            Filter = $Filter
+        }
         
-        #Add a new line
-        Out-File @Params -InputObject:""
-                
+        (Get-ChildItem @Params | Sort-Object LastWriteTime)[-1]
     }
 
-    #Copy the temporaryfile to the Destination.
-    Copy-Item -Path:$TemfileVariable -Destination:$FilePath
+    #test if an module with Name already exists
+    try {
+        $ModuleTest = Get-MostRecentFile -Path:$Destination -Filter:"$ModuleName*" -ErrorAction:Stop
+    }
+    catch {
+        Write-Warning "No File Found $ModuleName"
+    }
 
-    #Remove the temporaryfile to keep everything nice and tidy.
-    Remove-Item -Path:"$TemfileVariable"
+    if ($Null -eq $ModuleTest) {
+        Write-Verbose "File not found: $DestinationFullName"
+        #If FileName doesn't contain correct extension it will be added.
+        if (-not ($ModuleName -like "*1.0.0.psm1")) {
+            $ModuleName = "$ModuleName.1.0.0.psm1"
+        }
+    }
+    else {
+        #If Increment is set to Build,Minor or Major then update file version accordingly if not keep current Name.
+        switch ($Increment) {
+            None { $ModuleName = $ModuleTest }
+            Build { $ModuleName = $(Update-FileVersion -FileName:$($ModuleTest) -Build) }
+            Minor { $ModuleName = $(Update-FileVersion -FileName:$($ModuleTest) -Minor) }
+            Major { $ModuleName = $(Update-FileVersion -FileName:$($ModuleTest) -Major) }
+        }
+
+    }
+
+    #Tempory File in Temp location needs to be removed when finisched.
+    $TempFile = "$($env:TEMP)\$ModuleName"
+    
+    #Create Variable for Full Destination Path
+    $DestinationFullName = "$Destination\$ModuleName"
+        
+    #Get Source folder content.
+    $ListFiles = $((Get-ChildItem -Path:"$Functions" -Recurse -Filter:"*.ps1").FullName)
+
+    #Default parameters for writing to file
+    $Params = @{
+        FilePath = "$TempFile"
+        #Encoding = "utf8"
+        #Force    = $true
+    }
+    #Add info about last update and whome pushed it.
+    Out-File @Params -InputObject:"<$([char]0x0023)`nUpdated: $(Get-Date)`nPerformed by: $($env:USERName)`n$([char]0x0023)>"
+
+    #Add a new line
+    Out-File @Params -Append -InputObject:""
+
+    #Test if Source folder is empty.
+    if (-not ($null -eq $ListFiles)) {
+        $ListFiles | ForEach-Object {
+            $FunctionName = $((Split-Path -Path:$_ -Leaf).Replace(".ps1", ""))
+            Write-Verbose "Store function $FunctionName"
+            #Add #Region <VariableName> line
+            Out-File @Params -Append -InputObject:"$([char]0x0023)region $FunctionName"
+            #Add Function to Module
+            Out-File @Params -Append -InputObject:$(Get-Content -Path:"$_" -Raw)
+            #Add #EndRegion <VariableName> line
+            Out-File @Params -Append -InputObject:"$([char]0x0023)endregion $FunctionName"
+            #Add a new line
+            Out-File @Params -Append -InputObject:""
+                
+        }
+                
+        Write-Verbose "Copying $TempFile to $DestinationFullName"
+                
+        #If Destination folder isn't present create it.
+        if (-not (Test-Path "$Destination")) {
+            #Folder creation
+            New-Item -Path:$Destination -ItemType:Directory -Force
+        }
+
+
+        #Get-ChildItem -Path:"$DestinationFullName"
+
+        #Copy Local TempFile to destination.
+        Copy-Item -Path:$TempFile -Destination:"$DestinationFullName" -Force
+
+        #Remove LocalTempFile
+        Remove-Item -Path:$TempFile -Force
+    }
+
+
+            
+
+    else {
+        Write-Verbose "No Functions found to import"
+    }    
+        
+
 }
-else {
-    Write-Verbose "No Functions found to import"
-    exit
+
+#Splated overview to perform 
+$Params = @{
+    #Specify the location or your Functions
+    Functions   = "Location"
+    #Specify the location where to put the Module
+    Destination = "Destination"
+    #Actual Module name
+    ModuleName  = "PatchManagement"
+    #Select if build "None,Major,Minor & Build"
+    Increment   = None
 }
+
+Update-Module @Params
